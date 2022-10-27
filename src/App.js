31e496035186f1b9ad9dd7a8a2ae8b0016440db0
SmokeyNoom <110678072+smokeynoom@users.noreply.github.com>
2022-10-27 16:17:15 +0800

    Testing

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1> Halloween </h1>
-      <body>Hello </body>
-      </header>
-    </div> 
+    <head>
+      <title>Halloween Game</title>
+    </head>
+    
+    <body>
+        <header className="App-header">
+        <h1> Halloween </h1>
+        <body>Hello</body>
+        </header>
+    </body>
   );
 }
 
